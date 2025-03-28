cc09e96977f0521d28b62cb76511a16c81a8f762
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-28 21:54:38 +0300

    Updated sshot

--- modules/home/programs/sshot.nix
+++ modules/home/programs/sshot.nix
@@ -8,7 +8,7 @@ pkgs.writeShellScriptBin "sshot" ''
 	thumbnail_path=$(mktemp --suffix=.png)
 	convert "$screenshot_path" -resize 100x100 "$thumbnail_path"
 	
-	notify-send -t 2000 -i "$thumbnail_path" "Screenshot Captured" "Screenshot saved and copied to clipboard"
+	notify-send -t 2000 -i "$thumbnail_path" "  Screenshot Captured" "  Screenshot saved and copied to clipboard"
 	
 	rm "$screenshot_path" "$thumbnail_path"
 ''
