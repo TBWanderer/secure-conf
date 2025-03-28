4a0faba42509e3260761dcf62af230505a476fd1
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-28 21:50:56 +0300

    Added nix-ld

--- flake.nix
+++ flake.nix
@@ -4,14 +4,19 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     	stylix.url = "github:danth/stylix/release-24.11";	
+		nix-ld = {
+			url = "github:Mic92/nix-ld";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 		home-manager = {
 			url = "github:nix-community/home-manager/release-24.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		firefox.url = "github:nix-community/flake-firefox-nightly";
+		rust-overlay.url = "github:oxalica/rust-overlay";
 	};
 
-	outputs = { nixpkgs, home-manager, ... }@inputs: {
+	outputs = { nixpkgs, nix-ld, home-manager, ... }@inputs: {
 		nixosConfigurations = {
 			laptop = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
