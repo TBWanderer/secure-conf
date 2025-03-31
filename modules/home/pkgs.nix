4e3ac5b3ca6ab3f06656a2b8bf5653bdeb40ee14
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-31 13:11:26 +0300

    Added some pkgs

--- modules/home/pkgs.nix
+++ modules/home/pkgs.nix
@@ -1,8 +1,12 @@
 { pkgs, inputs, system, ... }:
 let unstable = import inputs.unstable { inherit system; }; in {
-	home.packages = with pkgs; [
-		inputs.firefox.packages.${pkgs.system}.firefox-nightly-bin
-
+	home.packages = with pkgs; with inputs; [
+		# Packages from inputs
+		firefox.packages.${pkgs.system}.firefox-nightly-bin
+		pabc-nix.packages.${pkgs.system}.default
+		
+		# User packages
+		vesktop
 		cliphist
 		libnotify
 		swaynotificationcenter
