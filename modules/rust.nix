46a7d838cf52866cc66006c2f91069288e2854d7
TBWanderer <evgenypodyryaka@yandex.ru>
2025-03-31 13:11:38 +0300

    Changed stable to nightly

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,8 +1,9 @@
 { pkgs, inputs, ... }: {
 	nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
 	environment.systemPackages = [
-		(pkgs.rust-bin.stable.latest.default.override {
-			extensions = [ "rust-analyzer" "rust-src" ];
-		})
+		(pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+			extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+			targets = [ "x86_64-apple-darwin" "x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu" ];
+		}))
 	];
 }
