ba5599ab4f365d306888e89f47a73a479d7bb3ca
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-20 18:46:00 +0300

    Moved to stable NVIDIA drivers

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -7,7 +7,7 @@
     	    powerManagement.finegrained = false;
     	    open = false;
     	    nvidiaSettings = true;
-    	    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    	    package = config.boot.kernelPackages.nvidiaPackages.stable;
     	};
     	services.xserver.videoDrivers = [ "nvidia" ];
 	};
