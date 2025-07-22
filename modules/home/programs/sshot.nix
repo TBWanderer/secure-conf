cd62ebfc7849803c383471be1c8baf55d10a6ba5
TBWanderer <125365236+TBWanderer@users.noreply.github.com>
2025-07-22 12:39:37 +0300

    Try to make new sshot script

--- modules/home/programs/sshot.nix
+++ modules/home/programs/sshot.nix
@@ -1,12 +1,16 @@
 { pkgs }:
 pkgs.writeShellScriptBin "sshot" ''
 	screenshot_path=$(mktemp --suffix=.png)
+	# screenshot_path_2=$(mktemp --suffix=.png)
 	slurp_area=$(${pkgs.slurp}/bin/slurp)
 	sleep 0.15
 
-	${pkgs.grim}/bin/grim -g "$slurp_area" "$screenshot_path"
-	
-	${pkgs.wl-clipboard}/bin/wl-copy < "$screenshot_path"
+	${pkgs.grim}/bin/grim -g "$slurp_area" "$screenshot_path" && ${pkgs.wl-clipboard}/bin/wl-copy < "$screenshot_path"
+	# ${pkgs.satty}/bin/satty \
+	# 	--filename "$screenshot_path_1" \
+	# 	--copy-command ${pkgs.wl-clipboard}/bin/wl-copy \
+	# 	--early-exit \
+	# 	--fullscreen
 	
 	thumbnail_path=$(mktemp --suffix=.png)
 	${pkgs.imagemagickBig}/bin/convert \
